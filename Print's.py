ab85eff095527e0553f9a87c00980d6e16e29f9d
AJ7-ME <aaravkjoshi2013@gmail.com>
2025-12-17 15:18:20 +1100

    First Commit

---
+++ Print's.py
@@ -0,0 +1,48 @@
+#activity 1
+#print a sentence
+print("welcome to the world of programming")
+
+#print a number
+print(4)
+
+#Next line after Print
+print("Hello Python \n")
+
+#pass multiple arguments for printing
+print("hello",5)
+
+#end argument of print statement
+print("welcome to ", end= "*")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
