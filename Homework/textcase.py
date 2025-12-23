08d7473e6134c0d18223f91f8f80f406b88acfa6
AJ7-ME <aaravkjoshi2013@gmail.com>
2025-12-23 23:49:16 +1100

    Homework. it was easy

---
+++ Homework/textcase.py
@@ -0,0 +1,8 @@
+
+text = str(input("Enter your Congratulations message: \n"))
+text = text.upper()
+print(text)
+# I didn't know which version you wanted so I did both 😁
+text = "Congratulations"
+text = text.upper()
+print(text)
