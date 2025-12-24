f7c7f89cbd2aebce6b54071762fcafba4130c5c8
AJ7-ME <aaravkjoshi2013@gmail.com>
2025-12-24 22:58:31 +1100

    yees

---
+++ Homework/Temp.py
@@ -0,0 +1,10 @@
+temp = int(input("What is the temperature today in Celsius? "))
+
+if temp <= 0:
+    print("It's freezing. Wear a heavy jacket, gloves, and a beanie.")
+if temp <= 10:
+    print("It's cold. Wear a jacket or hoodie.")
+if temp <= 20:
+    print("It's cool. A long-sleeve shirt or light jacket is good.")
+if temp <= 30:
+    print("It's hot. Wear light clothes and drink a LOT of water.")
