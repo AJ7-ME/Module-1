aaebbe2d03720f49e3a67805f1aa12483c86be1e
AJ7-ME <aaravkjoshi2013@gmail.com>
2025-12-29 15:12:08 +1100

    morbidly obese 🤣😂

---
+++ Class 6/And or project.py
@@ -0,0 +1,30 @@
+a = int(input("Enter first int value\n"))
+b = int(input("Enter second int value\n"))
+c = int(input("Enter third int value\n"))
+if a > b or a < c:
+    print("I am using or operator")
+elif a < b and a > c:
+    print("I am using and operator")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Class 6/Not equal.py
@@ -0,0 +1,20 @@
+print("This program checks if any integers are not/equal to each other.")
+a = int(input("Enter first int value\n"))
+b = int(input("Enter second int value\n"))
+c = int(input("Enter third int value\n"))
+
+print(a != b)
+print(b != c)
+
+a = "python"
+b = "coding"
+
+if a != b:
+    print(a, 'and', b, 'are not equal')
+
+
+a = 4
+b = 5
+
+if (a ==1) !=(b ==5):
+    print("One of the conditions is true")
