7e8f7c7c88b2c0ca30a573f1c2a23a2bea1b8435
AJ7-ME <aaravkjoshi2013@gmail.com>
2026-01-04 18:35:01 +1100

    hi

--- Homework/ASCII.py
+++ Homework/ASCII.py
@@ -1,24 +1,7 @@
-while True:
-    ch = input("Enter a single character: ")
+ch = input("Enter a single character: ")
 
-    if len(ch) != 1:
-        print("Invalid input! Please enter exactly ONE character.\n")
-        continue
-
-    if type(ch) is not str:
-        print("Invalid input! Input must be a string.\n")
-        continue
-
+if len(ch) != 1:
+    print("Please enter exactly ONE character.")
+else:
     print("You entered:", ch)
     print("ASCII value:", ord(ch))
-
-    if ch.isupper():
-        print("Character type: Uppercase letter")
-    elif ch.islower():
-        print("Character type: Lowercase letter")
-    elif ch.isdigit():
-        print("Character type: Digit")
-    else:
-        print("Character type: Special character")
-
-    break
--- Homework/Num_swap.py
+++ Homework/Num_swap.py
@@ -1,11 +1,9 @@
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = 1
-
 c = a
 a = b
 b = c
-
 print("After swapping:")
 print("a =", a)
 print("b =", b)
