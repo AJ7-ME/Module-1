7c8c01f8cb3929b913020be3db8899f39ce24fcc
AJ7-ME <aaravkjoshi2013@gmail.com>
2026-01-04 18:45:28 +1100

    bro commit and dont take 1hr

--- Homework/ASCII.py
+++ Homework/ASCII.py
@@ -1,7 +1,8 @@
-ch = input("Enter a single character: ")
+while True:
+    character = input("Enter a character: \n")
 
-if len(ch) != 1:
-    print("Please enter exactly ONE character.")
-else:
-    print("You entered:", ch)
-    print("ASCII value:", ord(ch))
+    if len(character) != 1:
+        print("Please enter exactly ONE character.")
+    else:
+        print("You entered:", character)
+        print("Your ASCII value is:", ord(character))
