8ad3bff54ded11dcb0414f0167954782e91d6f03
AJ7-ME <aaravkjoshi2013@gmail.com>
2026-01-07 16:14:22 +1100

    im laggy :(

--- Homework/ASCII.py
+++ Homework/ASCII.py
@@ -1,8 +1,9 @@
 while True:
-    character = input("Enter a character: \n")
+    for i in range(3):
+        character = input("Enter a character: \n")
 
-    if len(character) != 1:
-        print("Please enter exactly ONE character.")
-    else:
-        print("You entered:", character)
-        print("Your ASCII value is:", ord(character))
+        if len(character) != 1:
+            print("Please enter exactly ONE character.")
+        else:
+            print("You entered:", character)
+            print("Your ASCII value is:", ord(character))
