564dbfad8fffa0c6731b174fabb31ff27e0aed4e
AJ7-ME <aaravkjoshi2013@gmail.com>
2026-01-07 16:18:37 +1100

    im doing homework nut it is tedious :(

--- Homework/Alphabet checker.py
+++ Homework/Alphabet checker.py
@@ -1,12 +1,11 @@
 while True:
     ch = input("Enter a single character: \n")
-
     if len(ch) == 1:
         print("\nYou entered:", ch)
     else:
         print("\nInvalid input! You must enter ONE character.\n")
         continue
-    if ch.isalpha():
-        print(ch,"is an alphabet.\n")
+    if ('a' <= ch <= 'z') or ('A' <= ch <= 'Z'):
+        print(ch, "is an alphabet.\n")
     else:
-        print(ch,"is not an alphabet.\n")
+        print(ch, "is not an alphabet.\n")
